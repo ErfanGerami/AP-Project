7730104139e93c0deb730fffab682f52bf453edf
Shafagh Sepehr <shafaghsepehr2003@gmail.com>
2023-06-13 22:11:32 +0330

    replaced the filing functions in login and signup with new ones written in FileHandeling class

--- login.cpp
+++ login.cpp
@@ -9,24 +9,23 @@
 #include <QMediaPlayer>
 #include <QMediaPlaylist>
 #include "forgotmypassword.h"
-extern Player* MainPlayer;
+extern Player *MainPlayer;
 Login::Login(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::Login)
-{
-    ui->setupUi(this);
-    QPropertyAnimation* anim=new QPropertyAnimation(ui->label,"geometry");
-    anim->setDuration(1000);
-    anim->setStartValue(QRect(-110,-110,911,720));
-    anim->setEndValue(QRect(0,0,160,100));
-    anim->start();
-    QSound::play(":/sounds/sounds/laugh.wav");
-    QMediaPlayer* media=new QMediaPlayer();
-    QMediaPlaylist* playlist=new QMediaPlaylist;
-    playlist->addMedia(QUrl("qrc:/sounds/sounds/UiBackSound.mp3"));
-    media->setPlaylist(playlist);
-    media->setVolume(30);
-    media->play();
+	: QMainWindow(parent)
+	, ui(new Ui::Login) {
+	ui->setupUi(this);
+	QPropertyAnimation *anim = new QPropertyAnimation(ui->label, "geometry");
+	anim->setDuration(1000);
+	anim->setStartValue(QRect(-110, -110, 911, 720));
+	anim->setEndValue(QRect(0, 0, 160, 100));
+	anim->start();
+	QSound::play(":/sounds/sounds/laugh.wav");
+	QMediaPlayer *media = new QMediaPlayer();
+	QMediaPlaylist *playlist = new QMediaPlaylist;
+	playlist->addMedia(QUrl("qrc:/sounds/sounds/UiBackSound.mp3"));
+	media->setPlaylist(playlist);
+	media->setVolume(30);
+	media->play();
 
 
 
@@ -38,10 +37,9 @@ Login::Login(QWidget *parent)
 
 
 
-Login::~Login()
-{
+Login::~Login() {
 
-    delete ui;
+	delete ui;
 }
 
 
@@ -50,38 +48,31 @@ Login::~Login()
 
 
 
-void Login::on_start_clicked()
-{
-    SignUp* sign=new SignUp();
-    sign->show();
+void Login::on_start_clicked() {
+	SignUp *sign = new SignUp();
+	sign->show();
 }
 
 
-void Login::on_login_clicked()
-{
-    try{
-        auto vec=FileHandeling::read(ui->user->text()+".txt");
-        Game* prev;
-        if(vec[7]!="null")
-            prev=new Game(vec[8],vec[9]);
-        else
+void Login::on_login_clicked() {
+	try {
 
-            prev=nullptr;
-        MainPlayer=new Player(vec[0],vec[1],vec[2].toInt(),vec[3].toInt(),vec[4].toInt(),vec[5],vec[6],vec[7],prev);
+		MainPlayer = FileHandeling::file_read(ui->user->text(), ui->pass->text());
 
-    }catch(Errors err){
-        ui->statusbar->setStyleSheet("#statusbar{color:red;}");
-        ui->statusbar->showMessage("user not found or access denied");
-        QApplication::beep();
+	}
+	catch ( Errors err ) {
+		ui->statusbar->setStyleSheet("#statusbar{color:red;}");
+		//ui->statusbar->showMessage("user not found or access denied");
+		QMessageBox::critical(this, "ERROR", err.what());
+		QApplication::beep();
 
 
-    }
+	}
 }
 
 
-void Login::on_forgot_clicked()
-{
-    ForgotMyPassword* forgot_password=new ForgotMyPassword();
-    forgot_password->show();
+void Login::on_forgot_clicked() {
+	ForgotMyPassword *forgot_password = new ForgotMyPassword();
+	forgot_password->show();
 }
 
--- signup.cpp
+++ signup.cpp
@@ -1,68 +1,69 @@
 #include "signup.h"
 #include "ui_signup.h"
-SignUp::SignUp(QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::SignUp)
-{
-    ui->setupUi(this);
+SignUp::SignUp(QWidget *parent):
+	QDialog(parent),
+	ui(new Ui::SignUp) {
+	ui->setupUi(this);
 }
 
-SignUp::~SignUp()
-{
-    delete ui;
-}
-
-
-void SignUp::on_agree_stateChanged(int arg1)
-{
-    if(this->ui->agree->isChecked()){
-        this->ui->agree->setStyleSheet("#agree{color:white;font-size:20px;}");
-
-    }else{
-        this->ui->agree->setStyleSheet("#agree{color:red;font-size:20px;}");
-
-    }
+SignUp::~SignUp() {
+	delete ui;
 }
 
 
-void SignUp::on_start_clicked()
-{
-    QString pass=ui->passL->text();
-    QString user=ui->userL->text();
-    QString adr=ui->adrL->text();
-    QString phone_number=ui->phoneL->text();
-    QString confirm=ui->ConfirmL->text();
-    QString email=ui->emailL->text();
-    bool agreed=ui->agree->isChecked();
-    if(!agreed){
-        QMessageBox::critical(this,"ERROR","You should agree to the terms first");
-        return;
-    }
-    if(pass.indexOf('/')!=-1||user.indexOf('/')!=-1||adr.indexOf('/')!=-1||phone_number.indexOf('/')!=-1){
-        QMessageBox::critical(this,"ERROR","The queries should not contain the character '/'");
-        return;
-    }
-    if(FileHandeling::IsAPlayer(user)){
-        QMessageBox::critical(this,"ERROR","A player with that name already exists(or the name is restricted or access to files is denied)");
-        return;
-    }
-    if(confirm!=pass){
-        QMessageBox::critical(this,"ERROR","Confirmed password and password doesnt match");
-        return;
-    }
-    if(confirm!=pass){
-        QMessageBox::critical(this,"ERROR","Confirmed password and password doesnt match");
-        return;
-    }
-    if(email.indexOf('@')==-1){
-        QMessageBox::critical(this,"ERROR","email address is not valid");
-        return;
+void SignUp::on_agree_stateChanged(int arg1) {
+	if ( this->ui->agree->isChecked() ) {
+		this->ui->agree->setStyleSheet("#agree{color:white;font-size:20px;}");
 
-    }
+	}
+	else {
+		this->ui->agree->setStyleSheet("#agree{color:red;font-size:20px;}");
 
-    MainPlayer=new Player(user,pass,0,0,0,phone_number,adr,email,nullptr);
+	}
+}
 
-    FileHandeling::write(user,MainPlayer->Parse());
+
+void SignUp::on_start_clicked() {
+	QString pass = ui->passL->text();
+	QString user = ui->userL->text();
+	QString adr = ui->adrL->text();
+	QString phone_number = ui->phoneL->text();
+	QString confirm = ui->ConfirmL->text();
+	QString email = ui->emailL->text();
+	bool agreed = ui->agree->isChecked();
+
+
+	if ( !FileHandeling::is_players_file_open() )
+		throw Errors(Errors::file_openning_error);
+
+	if ( !agreed ) {
+		QMessageBox::critical(this, "ERROR", "You should agree to the terms first");
+		return;
+	}
+	/*if ( pass.indexOf('/') != -1 || user.indexOf('/') != -1 || adr.indexOf('/') != -1 || phone_number.indexOf('/') != -1 ) {
+		QMessageBox::critical(this, "ERROR", "The queries should not contain the character '/'");
+		return;
+	}*/
+	if ( !FileHandeling::is_user_unique(user) ) {
+		QMessageBox::critical(this, "ERROR", "A player with that name already exists(or the name is restricted or access to files is denied)");
+		return;
+	}
+	if ( confirm != pass ) {
+		QMessageBox::critical(this, "ERROR", "Confirmed password and password doesnt match");
+		return;
+	}
+	if ( email.indexOf('@') == -1 ) {
+		QMessageBox::critical(this, "ERROR", "email address is not valid");
+		return;
+
+	}
+
+	//MainPlayer = new Player(user, pass, 0, 0, 0, phone_number, adr, email, nullptr);// now player should login
+	//FileHandeling::write(user, MainPlayer->Parse());
+
+
+	Player *new_player = new Player(user, pass, 0, 0, 0, phone_number, adr, email, nullptr);
+	FileHandeling::file_write(new_player);
 
 
 }
