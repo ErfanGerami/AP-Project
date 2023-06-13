05fb39536dedddda522f7c7a500d620c0e4e7fe3
Shafagh Sepehr <shafaghsepehr2003@gmail.com>
2023-06-13 21:10:57 +0330

    added two more static error code one for incorrect password and the other for user not found

--- errors.cpp
+++ errors.cpp
@@ -1,13 +1,27 @@
 #include "errors.h"
 
-Errors::Errors()
-{
 
 
+Errors::Errors(int stat) {
+	this->stat = stat;
 }
-Errors::Errors(int stat)
-{
-    this->stat=stat;
+
+
+int Errors::GetError() { return stat; }
+
+
+QString Errors::what() {
+	switch ( stat ) {
+		case 0: return "file didn't open correctly";
+		case 1:	return "the credentials you are using are invalid";
+		case 2:	return "the credentials you are using are invalid";
+			//case 3:
+
+
+		default: return "error";
+
+	}
 }
-int Errors::GetError(){return stat;}
-int Errors::file_openning_error=0;
+int Errors::file_openning_error = 0;
+int Errors::player_not_found = 1;
+int Errors::incorrect_password = 2;
--- errors.h
+++ errors.h
@@ -1,17 +1,19 @@
 #ifndef ERRORS_H
 #define ERRORS_H
+#include <QString>
 
-
-class Errors
-{
+class Errors {
 public:
-    Errors();
-    Errors(int stat);
-    int GetError();
-    static int file_openning_error;
+	Errors() = default;
+	Errors(int stat);
+	int GetError();
+	QString what();
+	static int file_openning_error;
+	static int player_not_found;
+	static int incorrect_password;
 
 private:
-    int stat;
+	int stat;
 
 };
 #endif // ERRORS_H
